45ca2ba5baa9ebdb8ef9c2e528cb1aacb8cf1dcb
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2022-05-13 16:49:09 -0400

    front en

--- home-renter-client/src/App.js
+++ home-renter-client/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Hero from './components/Hero';
+import Popular from './components/Popular';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Hero />
+      <Popular />
     </div>
   );
 }
