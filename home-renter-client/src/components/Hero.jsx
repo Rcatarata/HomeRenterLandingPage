d474b2124a952bd79f1da5c69010081e0504fcee
Rcatarata <69015151+Rcatarata@users.noreply.github.com>
2022-05-14 18:30:39 -0400

    changing front end
    frontend

--- home-renter-client/src/components/Hero.jsx
+++ home-renter-client/src/components/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
             <img className='home1' src="https://images.unsplash.com/photo-1519999933293-fb5e79c3e9b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" alt="" />
         </div>
         <div className="right--hero">
-            <h1>Discover the ideal property here</h1>
+            <h1 className="hero--header">Discover the ideal property here</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. In, qui repellat! Blanditiis officiis facilis nemo architecto quos illo tempora hic?</p>
             <div className="white--container">
                 <div className="circle">
